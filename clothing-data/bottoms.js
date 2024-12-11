4e6ab76962a3f5e49f54afd1dab1832658554217
Toby <toby.j.lawrence@icloud.com>
2024-12-11 09:23:53 -0400

    put in links

--- clothing-data/bottoms.js
+++ clothing-data/bottoms.js
@@ -1,89 +1,209 @@
 export const mensBottoms = [
-    {
-        name: 'jeans',
-        imageSrc: 'https://m.media-amazon.com/images/I/61xsk77in4L._AC_SX679_.jpg',
-        moods: ["casual", "confident", "energetic", "rebellious"],
-        conditions: ["sunny", "clear", "partly cloudy", "cloudy", "overcast", "mist", "patchy rain possible", "light rain", "moderate rain at times", "patchy light snow", "light snow", "blowing snow"],
-        temperatures: ["mild", "cool", "cold"]
-    },
-    {
-        name: 'shorts',
-        imageSrc: 'https://m.media-amazon.com/images/I/71QPwKP9+WL._AC_SX569_.jpg',
-        moods: ["casual", "energetic", "playful", "confident"],
-        conditions: ["sunny", "clear", "partly cloudy", "cloudy", "overcast"],
-        temperatures: ["mild", "hot"]
-    },
-    {
-        name: 'sweatpants',
-        imageSrc: 'https://m.media-amazon.com/images/I/61gHFgDl9zL._AC_SX569_.jpg',
-        moods: ["casual", "confident", "energetic", "playful", "rebellious"],
-        conditions: ["sunny", "clear", "partly cloudy", "cloudy", "overcast", "mist", "patchy light rain", "light drizzle"],
-        temperatures: ["cool", "cold", "mild"]
-    },
-    {
-        name: 'chinos',
-        imageSrc: 'https://m.media-amazon.com/images/I/5139miWb51L._AC_SX679_.jpg',
-        moods: ["casual", "confident", "elegant", "playful", "formal"],
-        conditions: ["sunny", "clear", "partly cloudy", "cloudy", "overcast", "patchy light rain"],
-        temperatures:["cool", "mild", "hot"]
-    },
-    {
-        name: 'overalls',
-        imageSrc: 'https://m.media-amazon.com/images/I/51cDpQ69fKL._AC_SX679_.jpg',
-        moods: ["casual", "confident", "playful", "energetic", "rebellious"],
-        conditions: ["sunny", "clear", "partly cloudy", "cloudy", "overcast", "patchy light rain", "mist"],
-        temperatures: ["mild", "cool", "hot"]
-    },
-    {
-        name: 'thermal pants',
-        imageSrc: 'https://m.media-amazon.com/images/I/61EpTypVebL._AC_SX679_.jpg',
-        moods: ["casual", "confident", "energetic"],
-        conditions: ["sunny", "clear", "cloudy", "overcast", "mist", "patchy snow possible", "light snow", "heavy snow", "blizzard", "freezing fog"],
-        temperatures: ["cool", "cold"]
-    }
+  {
+    name: "jeans",
+    imageSrc: "https://m.media-amazon.com/images/I/61xsk77in4L._AC_SX679_.jpg",
+    linkSrc:
+      "https://www.amazon.ca/Levis-Regular-Medium-Stonewash-36x32/dp/B0018QS6N8",
+    moods: ["casual", "confident", "energetic", "rebellious"],
+    conditions: [
+      "sunny",
+      "clear",
+      "partly cloudy",
+      "cloudy",
+      "overcast",
+      "mist",
+      "patchy rain possible",
+      "light rain",
+      "moderate rain at times",
+      "patchy light snow",
+      "light snow",
+      "blowing snow",
+    ],
+    temperatures: ["mild", "cool", "cold"],
+  },
+  {
+    name: "shorts",
+    linkSrc:
+      "https://www.amazon.ca/Amazon-Essentials-Slim-Fit-Short-Olive/dp/B07F255MW5/",
+    imageSrc: "https://m.media-amazon.com/images/I/71QPwKP9+WL._AC_SX569_.jpg",
+    moods: ["casual", "energetic", "playful", "confident"],
+    conditions: ["sunny", "clear", "partly cloudy", "cloudy", "overcast"],
+    temperatures: ["mild", "hot"],
+  },
+  {
+    name: "sweatpants",
+    imageSrc: "https://m.media-amazon.com/images/I/61gHFgDl9zL._AC_SX569_.jpg",
+    linkSrc:
+      "https://www.amazon.ca/BALEAF-Sweatpants-Jogging-Workout-Straight/dp/B07Y1XBVXC/",
+    moods: ["casual", "confident", "energetic", "playful", "rebellious"],
+    conditions: [
+      "sunny",
+      "clear",
+      "partly cloudy",
+      "cloudy",
+      "overcast",
+      "mist",
+      "patchy light rain",
+      "light drizzle",
+    ],
+    temperatures: ["cool", "cold", "mild"],
+  },
+  {
+    name: "chinos",
+    imageSrc: "https://m.media-amazon.com/images/I/5139miWb51L._AC_SX679_.jpg",
+    linkSrc:
+      "https://www.amazon.ca/Levis-Standard-Taper-Chino-Mineral/dp/B07RP23J8G",
+    moods: ["casual", "confident", "elegant", "playful", "formal"],
+    conditions: [
+      "sunny",
+      "clear",
+      "partly cloudy",
+      "cloudy",
+      "overcast",
+      "patchy light rain",
+    ],
+    temperatures: ["cool", "mild", "hot"],
+  },
+  {
+    name: "overalls",
+    imageSrc: "https://m.media-amazon.com/images/I/51cDpQ69fKL._AC_SX679_.jpg",
+    linkSrc:
+      "https://www.amazon.ca/Carhartt-Mens-102776-Duck-Overalls/dp/B086XJNBKS",
+    moods: ["casual", "confident", "playful", "energetic", "rebellious"],
+    conditions: [
+      "sunny",
+      "clear",
+      "partly cloudy",
+      "cloudy",
+      "overcast",
+      "patchy light rain",
+      "mist",
+    ],
+    temperatures: ["mild", "cool", "hot"],
+  },
+  {
+    name: "thermal pants",
+    imageSrc: "https://m.media-amazon.com/images/I/61EpTypVebL._AC_SX679_.jpg",
+    linkSrc:
+      "https://www.amazon.ca/Hanes-Everyday-Lightweight-Thermal-Black/dp/B0DCWZSBK8/?th=1",
+    moods: ["casual", "confident", "energetic"],
+    conditions: [
+      "sunny",
+      "clear",
+      "cloudy",
+      "overcast",
+      "mist",
+      "patchy snow possible",
+      "light snow",
+      "heavy snow",
+      "blizzard",
+      "freezing fog",
+    ],
+    temperatures: ["cool", "cold"],
+  },
 ];
 
 export const womensBottoms = [
-    {
-        name: 'jeans',
-        imageSrc: 'https://m.media-amazon.com/images/I/51JVcfYQKQL._AC_SY741_.jpg',
-        moods: ["casual", "confident", "energetic", "rebellious"],
-        conditions: ["sunny", "clear", "partly cloudy", "cloudy", "overcast", "mist", "patchy rain possible", "light rain", "moderate rain at times", "patchy light snow", "light snow", "blowing snow"],
-        temperatures: ["mild", "cool", "cold"]
-    },
-    {
-        name: 'shorts',
-        imageSrc: 'https://m.media-amazon.com/images/I/61Cccp-zhHL._AC_SX679_.jpg',
-        moods: ["casual", "energetic", "playful", "confident"],
-        conditions: ["sunny", "clear", "partly cloudy", "cloudy", "overcast"],
-        temperatures: ["mild", "hot"]
-    },
-    {
-        name: 'leggings',
-        imageSrc: 'https://m.media-amazon.com/images/I/516G8WRO+BL._AC_SY741_.jpg',
-        moods: ["casual", "confident", "energetic", "playful", "rebellious"],
-        conditions: ["sunny", "clear", "partly cloudy", "cloudy", "overcast", "mist", "patchy light rain", "light drizzle"],
-        temperatures: ["cool", "cold", "mild"]
-    },
-    {
-        name: 'chinos',
-        imageSrc: 'https://m.media-amazon.com/images/I/61x3n2gP3gL._AC_SX679_.jpg',
-        moods: ["casual", "confident", "elegant", "playful", "formal"],
-        conditions: ["sunny", "clear", "partly cloudy", "cloudy", "overcast", "patchy light rain"],
-        temperatures:["cool", "mild", "hot"]
-    },
-    {
-        name: 'overalls',
-        imageSrc: 'https://m.media-amazon.com/images/I/61Luc0kXBeL._AC_SY741_.jpg',
-        moods: ["casual", "confident", "playful", "energetic", "rebellious"],
-        conditions: ["sunny", "clear", "partly cloudy", "cloudy", "overcast", "patchy light rain", "mist"],
-        temperatures: ["mild", "cool", "hot"]
-    },
-    {
-        name: 'thermal pants',
-        imageSrc: 'https://m.media-amazon.com/images/I/516Xsv-TX9L._AC_SX569_.jpg',
-        moods: ["casual", "confident", "energetic"],
-        conditions: ["sunny", "clear", "cloudy", "overcast", "mist", "patchy snow possible", "light snow", "heavy snow", "blizzard", "freezing fog"],
-        temperatures: ["cool", "cold"]
-    }
+  {
+    name: "jeans",
+    imageSrc: "https://m.media-amazon.com/images/I/51JVcfYQKQL._AC_SY741_.jpg",
+    linkSrc:
+      "https://www.amazon.ca/Levis-Womens-Original-Medium-Indigo/dp/B09Z1T241D",
+    moods: ["casual", "confident", "energetic", "rebellious"],
+    conditions: [
+      "sunny",
+      "clear",
+      "partly cloudy",
+      "cloudy",
+      "overcast",
+      "mist",
+      "patchy rain possible",
+      "light rain",
+      "moderate rain at times",
+      "patchy light snow",
+      "light snow",
+      "blowing snow",
+    ],
+    temperatures: ["mild", "cool", "cold"],
+  },
+  {
+    name: "shorts",
+    imageSrc: "https://m.media-amazon.com/images/I/61Cccp-zhHL._AC_SX679_.jpg",
+    linkSrc:
+      "https://www.amazon.ca/Dragon-Fit-Waisted-Drawstring-Athletic/dp/B09VGZ1FVH/",
+    moods: ["casual", "energetic", "playful", "confident"],
+    conditions: ["sunny", "clear", "partly cloudy", "cloudy", "overcast"],
+    temperatures: ["mild", "hot"],
+  },
+  {
+    name: "leggings",
+    imageSrc: "https://m.media-amazon.com/images/I/516G8WRO+BL._AC_SY741_.jpg",
+    linkSrc:
+      "https://www.amazon.ca/BALEAF-Leggings-Thermal-Waisted-Pocketed/dp/B07WYCL3D8",
+    moods: ["casual", "confident", "energetic", "playful", "rebellious"],
+    conditions: [
+      "sunny",
+      "clear",
+      "partly cloudy",
+      "cloudy",
+      "overcast",
+      "mist",
+      "patchy light rain",
+      "light drizzle",
+    ],
+    temperatures: ["cool", "cold", "mild"],
+  },
+  {
+    name: "chinos",
+    imageSrc: "https://m.media-amazon.com/images/I/61x3n2gP3gL._AC_SX679_.jpg",
+    linkSrc:
+      "https://www.amazon.ca/Tommy-Hilfiger-Womens-Hampton-Stretch/dp/B071VFKDBG",
+    moods: ["casual", "confident", "elegant", "playful", "formal"],
+    conditions: [
+      "sunny",
+      "clear",
+      "partly cloudy",
+      "cloudy",
+      "overcast",
+      "patchy light rain",
+    ],
+    temperatures: ["cool", "mild", "hot"],
+  },
+  {
+    name: "overalls",
+    imageSrc: "https://m.media-amazon.com/images/I/61Luc0kXBeL._AC_SY741_.jpg",
+    linkSrc:
+      "https://www.amazon.ca/Mmtochil-Adjustable-Spaghetti-Sleeveless-Classic/dp/B0CQPLMF7G/",
+    moods: ["casual", "confident", "playful", "energetic", "rebellious"],
+    conditions: [
+      "sunny",
+      "clear",
+      "partly cloudy",
+      "cloudy",
+      "overcast",
+      "patchy light rain",
+      "mist",
+    ],
+    temperatures: ["mild", "cool", "hot"],
+  },
+  {
+    name: "thermal pants",
+    imageSrc: "https://m.media-amazon.com/images/I/516Xsv-TX9L._AC_SX569_.jpg",
+    linkSrc:
+      "https://www.amazon.ca/INNERSY-Lightweight-Thermal-Bottoms-Leggings/dp/B0CBPX23SZ",
+    moods: ["casual", "confident", "energetic"],
+    conditions: [
+      "sunny",
+      "clear",
+      "cloudy",
+      "overcast",
+      "mist",
+      "patchy snow possible",
+      "light snow",
+      "heavy snow",
+      "blizzard",
+      "freezing fog",
+    ],
+    temperatures: ["cool", "cold"],
+  },
 ];
